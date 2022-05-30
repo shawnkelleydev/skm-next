52d3287056f67ec0d66d86593767b28cf0f7cfff
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-05-30 15:26:33 -0600

    Add performance and piece data

--- utils/constants/data.js
+++ utils/constants/data.js
@@ -6,8 +6,139 @@ export const meta = {
   AUTHOR: 'Shawn Kelley',
   CURRENT_YEAR: getYear(new Date()),
   DESCRIPTION:
-    'The digital home of musician Shawn Kelley, composer of Urban Suite for Tuba Quartet.',
-  KEYWORDS:
-    'shawn, kelley, kelly, sean, shaun, tuba, quartet, composer, musician',
+    'The digital home of composer Shawn Kelley, creator of Urban Suite for Tuba Quartet.',
+  KEYWORDS: 'shawn, kelley, tuba, quartet, composer, urban, suite',
   TITLE: 'Shawn Kelley',
 }
+
+export const quotes = [
+  {
+    text: 'A lovely work',
+    speaker: 'Iwan Fox, 4barsrest.com',
+  },
+  {
+    text: 'The composer employs simple but ingenious compositional techniques',
+    speaker: 'Jason Byrnes, University of Northern Colorado',
+  },
+  {
+    text: 'A work that has no dull moments and provides plenty of contrasts to entertain players and audience alike',
+    speaker: 'Jason Byrnes, University of Northern Colorado',
+  },
+  {
+    text: 'This is a well-crafted work that is FUN',
+    speaker: 'Jason Byrnes, University of Northern Colorado',
+  },
+]
+
+export const institutions = [
+  'Eastman School of Music',
+  'University of Cincinnati College-Conservatory of Music',
+  'Penn State University School of Music',
+  'University of Michigan School of Music',
+  'Michigan State University College of Music',
+  'University of Georgia Hugh Hodgson School of Music',
+  'University of Tennessee School of Music',
+  'University of Oregon School of Music and Dance',
+  'University of Arizona Fred Fox School of Music',
+  'Ithaca College School of Music',
+  'University of Nevada Las Vegas School of Music',
+  'Texas Tech University School of Music',
+  'Florida State University College of Music',
+  'International Tuba Euphonium Conference',
+  'Army Band Tuba Euphonium Workshop',
+  'Northeast Regional Tuba Euphonium Conference',
+  'Southeast Regional Tuba Euphonium Conference',
+  'Southwest Regional Tuba Euphonium Conference',
+  'Euphonix',
+  'Funkaru',
+  'III Marine Expeditionary Force Band',
+  'When Tuba Come One',
+]
+
+export const recordings = [
+  {
+    title: 'Funkaru!',
+    artist: 'Funkaru',
+    img: 'https://www.euphoniumstore.net/files/uploads/2020/10/1602796162_funkaru+cover.jpg',
+  },
+  {
+    title: 'Brink',
+    artist: 'Euphonix',
+    img: 'https://www.euphoniumstore.net/files/EUPHONIUMSTORE/IMAGES/brink_cover-det_1342087912.jpeg',
+  },
+  {
+    title: 'ITEC 2014',
+    artist: 'Penn State Tuba-Euphonium Ensemble',
+    vid: 'https://www.youtube.com/embed/2pgTT9XJVpE',
+  },
+  {
+    title: 'Army Band Tuba Euphonium Workshop',
+    artist: 'Florida State University Tuba & Euphonium Octet',
+    vid: 'https://www.youtube.com/embed/zp8GGMxm4x0?start=911',
+  },
+  {
+    artist: 'When Tuba Come One',
+    vid: 'https://www.youtube.com/embed/cLKJEqs6oko',
+  },
+  {
+    title: 'SWRTEC 2013',
+    artist: 'Las Tubas de Tucson',
+    vid: 'https://www.youtube.com/embed/DOqt5JIXdqw',
+  },
+]
+
+export const pieces = [
+  {
+    id: 'urban-suite',
+    title: 'urban suite',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/413g0ycJ9eL._SY344_BO1,204,203,200_.jpg',
+    publisher: { name: 'cimarron music', year: 2009 },
+    ensembleType: 'tuba quartet',
+    instrumentation: 'eett',
+    duration: 12,
+    difficulty: 'advanced',
+    highlights: [
+      'Fun and Beautiful',
+      'Challenging for all players',
+      'Finalist for 2010 Harvey Phillips Award',
+    ],
+    completionYear: 2008,
+    premier: {
+      location: 'Eastman School of Music',
+      year: 2009,
+      personel: ['john marcellus', 'mark kellogg', 'don harry', 't.j. ricer'],
+    },
+    historicalNotes: [
+      'Completed in 2008, *Urban* *Suite* was premiered by faculty at Eastman School of Music in 2009 and subsequently made its way across the US.  It was also performed in Europe and Japan.',
+      '*Urban* *Suite* was used in the 2010 ITEC Quartet Competition semi-final round and was a finalist for the 2010 Harvey Phillips Award.',
+    ],
+    programNotes: [
+      '*Urban* *Suite* is a story-celebration of life pursued with electric curiosity and restless uncertainty.',
+    ],
+    externalBuyUrl: 'https://www.cimarronmusic.com/urban-suite-8504',
+  },
+  {
+    id: 'an-off-day',
+    title: 'an off day',
+    img: 'https://www.cimarronmusic.com/media/catalog/product/cache/6517c62f5899ad6aa0ba23ceb3eeff97/c/m/cm5016cover_2.png',
+    publisher: { name: 'cimarron music', year: 2021 },
+    ensembleType: 'tuba quartet',
+    instrumentation: 'eett',
+    duration: 8,
+    difficulty: 'advanced',
+    completionYear: 2020,
+    externalBuyUrl: 'https://www.cimarronmusic.com/anoffday',
+  },
+  {
+    id: 'home-suite-home',
+    title: 'home suite home',
+    img: 'https://www.lowbrassmusic.com/media/catalog/product/cache/6517c62f5899ad6aa0ba23ceb3eeff97/c/m/cm5013cover_2.png',
+    publisher: { name: 'cimarron music', year: 2021 },
+    ensembleType: 'tuba quartet',
+    instrumentation: 'eett',
+    duration: 12,
+    difficulty: 'advanced',
+    completionYear: 2020,
+    externalBuyUrl: 'https://www.cimarronmusic.com/homesuitehome',
+  },
+]
